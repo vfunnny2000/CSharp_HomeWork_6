211ad966920ed4c5932a2379ec74c4f56d1c495a
vfunny2000@rambler.ru <vfunny2000@rambler.ru>
2022-08-26 11:29:34 +0300

    added tasks 41, 43, 44, 45

---
+++ Task44/Program.cs
@@ -0,0 +1,61 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. 
+// Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+void FillFibonacciArray(int[] fibonacciArrayToFill)
+{
+    fibonacciArrayToFill[0] = 0;
+    fibonacciArrayToFill[1] = 1;
+    for (int i = 2; i < fibonacciArrayToFill.Length; i++)
+    {
+        fibonacciArrayToFill[i]=fibonacciArrayToFill[i-1]+
+                                fibonacciArrayToFill[i-2];
+    }
+}
+void PrintArray(int[] ArrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < ArrayToPrint.Length; i++)
+    {
+        Console.Write($"{ArrayToPrint[i]}");
+        if (i < ArrayToPrint.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int InputN(int border)
+{
+    int NumberToInput = 0;
+    do
+    {
+        Console.Write($"Введите число больше {border}: ");
+        NumberToInput = Convert.ToInt32(Console.ReadLine());
+        if (NumberToInput <= border)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Ошибка! Нажмите любую клавишу и повторите ввод!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        else
+        {
+            return NumberToInput;
+        }
+    } while (true);
+}
+
+Console.Clear();
+Console.WriteLine("Для вывода первых N чисел Фибоначчи");
+int nFibonacci = InputN(2);
+
+int[] fibonacciArray = new int[nFibonacci];
+FillFibonacciArray(fibonacciArray);
+
+Console.WriteLine();
+PrintArray(fibonacciArray);
---
+++ Task45/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива
+//  с помощью поэлементного копирования.
+// void PrintArray(int[] arrayToPrint)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arrayToPrint.Length; i++)
+//     {
+//         Console.Write("{0}", arrayToPrint[i]);
+//         if (i < arrayToPrint.Length - 1)
+//         {
+//             Console.Write("   ");
+//         }
+//     }
+//     Console.WriteLine("]");
+// }
+
+// void FillArray(int[] arrayToFill, int diviation)
+// {
+//     for (int i = 0; i < arrayToFill.Length; i++)
+//         arrayToFill[i] = new Random().Next(-diviation, diviation + 1);
+// }
+
+// void CopyArray(int[] arrayToCopy, int[] arrayToFill)
+// {
+//     for (int i = 0; i < arrayToCopy.Length; i++)
+//         arrayToFill[i] = arrayToCopy[i];
+// }
+
+
+// int arraySize = 0;
+
+// arraySize = new Random().Next(5, 20);
+// int[] firstArray = new int[arraySize];
+// int[] secondArray = new int[arraySize];
+// FillArray(firstArray, 20);
+// CopyArray(firstArray, secondArray);
+// Console.Clear();
+
+// Console.WriteLine("Первый массив:");
+// PrintArray(firstArray);
+// Console.WriteLine();
+// Console.ForegroundColor = ConsoleColor.Green;
+// Console.WriteLine("Второй массив - копия первого:");
+// PrintArray(secondArray);
+// Console.ResetColor();
+
+
+
+
